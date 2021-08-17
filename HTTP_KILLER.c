7efd68f58616f0384b2fc17c93474be6efb3d321
XIANG-LEE <71392262+Jormungand-Stark@users.noreply.github.com>
2021-08-17 22:52:41 +0800

    Update HTTP_KILLER.c

--- HTTP_KILLER.c
+++ HTTP_KILLER.c
@@ -1,3 +1,20 @@
+/*
+* (C) Radim Kolar 1997-2004
+* This is free software, see GNU Public License version 2 for
+* details.
+*
+* Simple forking WWW Server benchmark:
+*
+* Usage:
+*   ./HTTP_KILL --help
+*
+* Return codes:
+*    0 - sucess
+*    1 - benchmark failed (server is not on-line)
+*    2 - bad param
+*    3 - internal error, fork failed
+* 
+*/ 
 #include"Socket.c"
 #include<unistd.h>
 #include<stdio.h>
